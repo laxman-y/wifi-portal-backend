9b4289b940e9ac75fb3b8e196cbc04aa0e4c0c93
laxman-y <laxmanyadav.dev@gmail.com>
2026-01-10 13:48:10 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,9 +7,6 @@ const authMiddleware = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
-router.post("/captive/login", (req, res) => {
-  return res.json({ test: "captive route works" });
-});
 
 router.post("/captive/login", async (req, res) => {
   try {

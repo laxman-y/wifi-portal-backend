fdfbf85e47d221e1f554a8eff5caced1c290c35e
laxman-y <laxmanyadav.dev@gmail.com>
2026-01-16 15:05:56 +0530

    Update StudentV2.js

--- models/StudentV2.js
+++ models/StudentV2.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
 
-const ShiftSchema = new mongoose.Schema(
-  {
-    start: String, // "06:00"
-    end: String    // "11:00"
-  },
-  { _id: false }
-);
-
 const StudentV2Schema = new mongoose.Schema(
   {
-    name: String,
-    mobile: { type: String, unique: true },
+    name: { type: String, required: true },
 
-    // 🔥 AUTO-BINDED
-    mac: { type: String, default: null, index: true },
+    // 🔐 silently bound
+    mac: { type: String, required: true, unique: true },
 
-    shifts: [ShiftSchema],
+    shifts: [
+      {
+        start: String, // "06:00"
+        end: String    // "11:00"
+      }
+    ],
 
-    // Attendance meta
     lastSeen: Date
   },
   { timestamps: true }
